1d6a7c003f551f24b0e02826732770923f4c301e
Moussekateer <moussa.uk@gmail.com>
2014-02-25 05:47:09 +0000

    add support for tmdb agent for letterboxd links

--- plugins/letterboxd.js
+++ plugins/letterboxd.js
@@ -1,6 +1,6 @@
-function constructLetterboxdLink(imdb_id) {
+function constructLetterboxdLink(id, agent) {
     var logo_url = "http://primer.cf.letterboxd.com/logos/letterboxd-logo-neg-rgb.png";
-    var letterboxd_link = "http://letterboxd.com/imdb/" + imdb_id;
+    var letterboxd_link = "http://letterboxd.com/" + agent + "/" + id;
 
     // construct link
     var letterboxd_element_link = document.createElement("a");
@@ -25,15 +25,31 @@ function createLetterboxdLink(xml) {
         return;
     }
 
-    // grab imdb id
-    debug("letterboxd plugin: Grabbing imdb id");
-    var imdb_agent = xml.getElementsByTagName("MediaContainer")[0].getElementsByTagName("Video")[0].getAttribute("guid");
-    var imdb_id = imdb_agent.match(/^com.plexapp.agents.imdb:\/\/(.+)\?/)[1];
-    debug("letterboxd plugin: Grabbed imdb id - " + imdb_id);
+    // grab tmdb/imdb id
+    debug("letterboxd plugin: Grabbing tmdb/imdb id");
+    var imdb_id;
+    var tmdb_id;
+    var agent = xml.getElementsByTagName("MediaContainer")[0].getElementsByTagName("Video")[0].getAttribute("guid");
+    // check if using the freebase metadata agent
+    if (/com.plexapp.agents.imdb/.test(agent)) {
+        imdb_id = agent.match(/^com.plexapp.agents.imdb:\/\/(.+)\?/)[1];
+        debug("letterboxd plugin: imdb id found - " + imdb_id);
+    }
+    // check if using the movie database metadata agent
+    else if (/com.plexapp.agents.themoviedb/.test(agent)) {
+        tmdb_id = agent.match(/^com.plexapp.agents.themoviedb:\/\/(.+)\?/)[1];
+        debug("letterboxd plugin: tmdb id found - " + tmdb_id);
+    }
 
     // create letterboxd link element
     debug("letterboxd plugin: Constructing letterboxd link");
-    var letterboxd_element = constructLetterboxdLink(imdb_id);
+    var letterboxd_element;
+    if (imdb_id) {
+        letterboxd_element = constructLetterboxdLink(imdb_id, "imdb");
+    }
+    else if (tmdb_id) {
+        letterboxd_element = constructLetterboxdLink(tmdb_id, "tmdb");
+    }
 
     // insert letterboxd link element to bottom of metadata container
     debug("letterboxd plugin: Inserting letterboxd link into page");

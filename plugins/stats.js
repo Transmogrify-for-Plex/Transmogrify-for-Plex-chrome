7a3e067f04204c20adf1ab4a49c4c9ba864b23d3
Moussekateer <moussa.uk@gmail.com>
2014-10-15 03:49:07 +0100

    add code for firefox for opening stats page

--- plugins/stats.js
+++ plugins/stats.js
@@ -22,5 +22,15 @@ stats = {
         list_element.appendChild(stats_link);
 
         nav_bar_right.insertBefore(list_element, nav_bar_right.firstChild);
+
+        // event handler for firefox to open stats page
+        var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        if (is_firefox) {
+            try {
+                stats_link.addEventListener("click", utils.openStatsPage, false);
+            }
+            catch(e) {
+            }
+        }
     }
 }
